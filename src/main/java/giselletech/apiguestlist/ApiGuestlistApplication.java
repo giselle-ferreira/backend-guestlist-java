e5f8f0b6fa7195a781683dead7c0359eb7894c01
Giselle Ferreira <giselle.fs@gmail.com>
2023-10-18 01:29:19 -0300

    Initial Commit

---
+++ src/main/java/giselletech/apiguestlist/ApiGuestlistApplication.java
@@ -0,0 +1,13 @@
+package giselletech.apiguestlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGuestlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGuestlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/giselletech/apiguestlist/ApiGuestlistApplicationTests.java
@@ -0,0 +1,13 @@
+package giselletech.apiguestlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGuestlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
